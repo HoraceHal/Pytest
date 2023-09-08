460424a19a829384497303fcd6071ff3baaeb447
lyy79 <lyy7595@163.com>
2023-09-08 16:45:09 +0800

    Changes

--- Day01-15/如果 否则 if-else.py
+++ Day01-15/如果 否则 if-else.py
@@ -1,15 +1,29 @@
 # if——else
 
 # 定义一个变量
-a = not True
-if a:
-    print(1)
-else:
-    print(2)
+# a = not True
+# if a:
+#     print(1)
+# else:
+#     print(2)
+#
+#
+# had = int(input('输入身高：'))
+# if had <= 150:
+#     print('不能进去')
+# else:
+#     print('可以进去')
 
 
-had = int(input('输入身高：'))
-if had <= 150:
-    print('不能进去')
+score = 89
+
+if 90 >= score <= 100:
+    print('优秀')
+elif 80 >= score < 90:
+    print('良好')
+elif 70 >= score < 80:
+    print('一般')
+elif 60 >= score < 70:
+    print('及格')
 else:
-    print('可以进去')
+    print('不及格')
